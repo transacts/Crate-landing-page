003fbfd6c775eda0351fbcac0077b0e759245add
transacts <119990253+transacts@users.noreply.github.com>
2025-08-16 00:30:40 -0400

    Update hero-section-1.tsx

--- components/blocks/hero-section-1.tsx
+++ components/blocks/hero-section-1.tsx
@@ -29,7 +29,7 @@ export function HeroSection() {
                                 <InteractiveHoverButton
                                   text="Start Now"
                                   defaultActive={true}
-                                  onClick={() => window.location.href = 'https://app.cratedashboard.com/login'}
+                                  onClick={() => router.push('https://app.cratedashboard.com/login')}
                                 />
                                 <InteractiveHoverButton
                                   text="Book a Demo"
